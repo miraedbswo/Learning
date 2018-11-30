2b9ce402936961fff8b8fc16bb837492a250fd63
Goming <miraedbswo@gmail.com>
2018-11-30 15:05:50 +0900

    restore files

--- pymongo.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient('localhost', 27017)
-client = MongoClient('mongodb://localhost:27017')
-
-db = client.test_database
-db = client['test-database']
-# you can use dictionary style
-
